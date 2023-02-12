7cf1024331467b2a9ee23372130c5ef75563a274
Omprakash <omdhanurkar444@gmail.com>
2023-02-12 23:49:37 +0530

    Movie

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,9 +6,6 @@ const Movie = (props) => {
             <img src={props.img} alt='' />
             <a href={props.link} ><p>{props.title}</p></a>
             <p>{props.year}</p>
-    
-
-
         </div>
     )
 }
